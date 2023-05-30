c58a03fd24aa0c3ac853f9760e28296368ddcf32
dsilvau <125933418+dsilvau@users.noreply.github.com>
2023-05-30 13:16:03 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -7,14 +7,32 @@ correctamente. Tenga en cuenta datos faltantes y duplicados.
 
 """
 import pandas as pd
-
+from datetime import datetime
+import re
 
 def clean_data():
+    df = pd.read_csv("solicitudes_credito.csv", sep=";", index_col=0)
 
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
+    df.dropna(axis=0, inplace=True)
+    df.drop_duplicates(inplace=True)
 
-    #
-    # Inserte su código aquí
-    #
+    columns_to_lower = ['sexo', 'tipo_de_emprendimiento', 'idea_negocio', 'línea_credito', 'barrio']
+    columns_to_replace = ['idea_negocio', 'barrio']
 
+    for column in columns_to_lower:
+        df[column] = df[column].str.lower()
+        df[column] = df[column].apply(lambda x: x.replace('_', ' '))
+        df[column] = df[column].apply(lambda x: x.replace('-', ' '))
+
+    for column in columns_to_replace:
+        df[column] = df[column].str.replace('-', '').str.replace('_', '').str.replace(' ', '').str.replace('.', '')
+
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace(r'\$|,|\..*$', '').astype(int)
+    df['comuna_ciudadano'] = df['comuna_ciudadano'].astype(float)
+
+    date_format = "%Y/%m/%d" if len(re.findall("^\d+/", df['fecha_de_beneficio'].iloc[0])[0]) - 1 == 4 else "%d/%m/%Y"
+    df['fecha_de_beneficio'] = pd.to_datetime(df['fecha_de_beneficio'], format=date_format)
+
+    df.drop_duplicates(inplace=True)
     return df
+
