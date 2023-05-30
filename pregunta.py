be8716db7012911aef29a4b29eaea6402e74397d
dsilvau <125933418+dsilvau@users.noreply.github.com>
2023-05-30 14:04:02 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -1,80 +1,78 @@
-"""
-Limpieza de datos usando Pandas
------------------------------------------------------------------------------------------
-
-Realice la limpieza del dataframe. Los tests evaluan si la limpieza fue realizada 
-correctamente. Tenga en cuenta datos faltantes y duplicados.
-
-"""
 import pandas as pd
 from datetime import datetime
 import re
 
 def clean_data():
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
+    data = pd.read_csv("solicitudes_credito.csv", sep=";")
 
-    df.sexo = df.sexo.str.lower()
+    data.sexo = data.sexo.str.lower()
 
-    df.tipo_de_emprendimiento = df.tipo_de_emprendimiento.str.lower()
+    data.tipo_de_emprendimiento = data.tipo_de_emprendimiento.str.lower()
 
-    df.idea_negocio = df.idea_negocio.str.lower()
-    df.idea_negocio = df.idea_negocio.apply(lambda x : x.replace('-',' '))
-    df.idea_negocio = df.idea_negocio.apply(lambda x : x.replace('_',' '))
+    data.idea_negocio = data.idea_negocio.str.lower()
+    data.idea_negocio = data.idea_negocio.apply(lambda x : x.replace('-',' '))
+    data.idea_negocio = data.idea_negocio.apply(lambda x : x.replace('_',' '))
 
-    df.barrio = df.barrio.astype(str)
-    df.barrio = df.barrio.str.lower()
-    df.barrio = df.barrio.apply(lambda x : x.replace('-', ''))
-    df.barrio = df.barrio.apply(lambda x : x.replace('_', ''))
-    df.barrio = df.barrio.apply(lambda x : x.replace(' ', ''))
-    df.barrio = df.barrio.apply(lambda x : x.replace('.', ''))
+    data.barrio = data.barrio.astype(str)
+    data.barrio = data.barrio.str.lower()
+    data.barrio = data.barrio.apply(lambda x : x.replace('-', ''))
+    data.barrio = data.barrio.apply(lambda x : x.replace('_', ''))
+    data.barrio = data.barrio.apply(lambda x : x.replace(' ', ''))
+    data.barrio = data.barrio.apply(lambda x : x.replace('.', ''))
 
-    df.estrato = df.estrato.astype(int)
+    data.estrato = data.estrato.astype(int)
 
-    df.comuna_ciudadano = df.comuna_ciudadano.astype(int)
+    data.comuna_ciudadano = data.comuna_ciudadano.astype(int)
 
-    df.monto_del_credito = df.monto_del_credito.apply(lambda x : x.replace('$',''))
-    df.monto_del_credito = df.monto_del_credito.apply(lambda x : x.replace(',',''))
-    df.monto_del_credito = df.monto_del_credito.apply(lambda x : x.replace('.00',''))
-    df.monto_del_credito = df.monto_del_credito.astype(int)
+    data.monto_del_credito = data.monto_del_credito.apply(lambda x : x.replace('$',''))
+    data.monto_del_credito = data.monto_del_credito.apply(lambda x : x.replace(',',''))
+    data.monto_del_credito = data.monto_del_credito.apply(lambda x : x.replace('.00',''))
+    data.monto_del_credito = data.monto_del_credito.astype(int)
 
-    df.fecha_de_beneficio = df.fecha_de_beneficio.apply(lambda x : x.replace('/', '-'))
-    df.fecha_de_beneficio = pd.to_datetime(df.fecha_de_beneficio,
+    data.fecha_de_beneficio = data.fecha_de_beneficio.apply(lambda x : x.replace('/', '-'))
+    data.fecha_de_beneficio = pd.to_datetime(data.fecha_de_beneficio,
                                        infer_datetime_format=True)
 
-    df.línea_credito = df.línea_credito.str.lower()
-    df.línea_credito = df.línea_credito.apply(lambda x : x.replace('_', ''))
-    df.línea_credito = df.línea_credito.apply(lambda x : x.replace('-', ''))
-    df.línea_credito = df.línea_credito.apply(lambda x : x.replace(' ', ''))
-    df.línea_credito = df.línea_credito.apply(lambda x : x.replace('.', ''))
+    data.línea_credito = data.línea_credito.str.lower()
+    data.línea_credito = data.línea_credito.apply(lambda x : x.replace('_', ''))
+    data.línea_credito = data.línea_credito.apply(lambda x : x.replace('-', ''))
+    data.línea_credito = data.línea_credito.apply(lambda x : x.replace(' ', ''))
+    data.línea_credito = data.línea_credito.apply(lambda x : x.replace('.', ''))
 
     # Final setup
 
-    df = df[df.barrio != "nan"]
+    data = data[data.barrio != "nan"]
 
-    df = df[df["barrio"].str.contains("¿") == False]
-    df.drop(columns=["Unnamed: 0"], inplace=True)
+    data = data[data["barrio"].str.contains("¿") == False]
+    data.drop(columns=["Unnamed: 0"], inplace=True)
 
-    df.dropna(inplace=True)
-    df.drop_duplicates(inplace=True)
+    data.dropna(inplace=True)
+    data.drop_duplicates(inplace=True)
 
-    df = pd.read_csv("solicitudes_credito.csv", sep=";", index_col = 0)
+    #print(data.sexo.value_counts().to_list())
+    #print(data.tipo_de_emprendimiento.value_counts().to_list())
+    #print(data.línea_credito.value_counts().to_list())
+    # data = data[data.barrio == "guayabal"]
+    # print(data.línea_credito.value_counts())
+
+    data = pd.read_csv("solicitudes_credito.csv", sep=";", index_col=0)
     
-    df.dropna(axis = 0, inplace = True)
-    df.drop_duplicates(inplace = True)
+    data.dropna(axis=0, inplace=True)
+    data.drop_duplicates(inplace=True)
 
     for columna in ['sexo', 'tipo_de_emprendimiento', 'idea_negocio', 'línea_credito', 'barrio']:
-        df[columna] = df[columna].str.lower()
-        df[columna] = df[columna].apply(lambda x: x.replace('_', ' '))
-        df[columna] = df[columna].apply(lambda x: x.replace('-', ' '))
+        data[columna] = data[columna].str.lower()
+        data[columna] = data[columna].apply(lambda x: x.replace('_', ' '))
+        data[columna] = data[columna].apply(lambda x: x.replace('-', ' '))
 
-    df['monto_del_credito'] = df['monto_del_credito'].str.replace("\$[\s*]", "")
-    df['monto_del_credito'] = df['monto_del_credito'].str.replace(",", "")
-    df['monto_del_credito'] = df['monto_del_credito'].str.replace("\.00", "")
-    df['monto_del_credito'] = df['monto_del_credito'].astype(int)
+    data['monto_del_credito'] = data['monto_del_credito'].str.replace("\$[\s*]", "")
+    data['monto_del_credito'] = data['monto_del_credito'].str.replace(",", "")
+    data['monto_del_credito'] = data['monto_del_credito'].str.replace("\.00", "")
+    data['monto_del_credito'] = data['monto_del_credito'].astype(int)
     
-    df['comuna_ciudadano'] = df['comuna_ciudadano'].astype(float)
+    data['comuna_ciudadano'] = data['comuna_ciudadano'].astype(float)
 
-    df['fecha_de_beneficio'] = df['fecha_de_beneficio'].apply(lambda x: datetime.strptime(x, "%Y/%m/%d") if (len(re.findall("^\d+/", x)[0]) - 1) == 4 else datetime.strptime(x, "%d/%m/%Y"))
+    data['fecha_de_beneficio'] = data['fecha_de_beneficio'].apply(lambda x: datetime.strptime(x, "%Y/%m/%d") if (len(re.findall("^\d+/", x)[0]) - 1) == 4 else datetime.strptime(x, "%d/%m/%Y"))
 
-    df.drop_duplicates(inplace = True)
-    return df
+    data.drop_duplicates(inplace=True)
+    return data
